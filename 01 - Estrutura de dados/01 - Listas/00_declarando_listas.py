172261952ae1d42c1dcf0e31fb2c1b2120b769b1
RailanderJ <36487179+RailanderJoseSantos@users.noreply.github.com>
2023-08-24 22:36:38 -0300

    Anotações feitas no submodulo '01 - Listas'

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -1,13 +1,13 @@
 frutas = ["laranja", "maca", "uva"]
 print(frutas)
 
-frutas = []
+frutas = [] #podemos declarar lista vazia.
 print(frutas)
 
-letras = list("python")
+letras = list("python")#passando string pelo construct list ira entrar letra por letra separadamente
 print(letras)
 
-numeros = list(range(10))
+numeros = list(range(10))#mesma logica da string, cada numero entra separadamente na lista
 print(numeros)
 
 carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
